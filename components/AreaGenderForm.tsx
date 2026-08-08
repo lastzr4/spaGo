5cc9caf797f74ce27caf6a2c252cf7bfeff44b7e
lastzr4 <lastzr4@gmail.com>
2026-08-08 17:57:41 +0800

    Fix map pin icon overlapping placeholder text in area dropdown

--- components/AreaGenderForm.tsx
+++ components/AreaGenderForm.tsx
@@ -26,6 +26,7 @@ export default function AreaGenderForm() {
           <MapPinIcon className="pointer-events-none absolute left-4 top-1/2 h-4 w-4 -translate-y-1/2 text-brand-300" />
           <select
             className="input appearance-none pl-11"
+            style={{ paddingLeft: "2.75rem" }}
             value={area}
             onChange={(e) => setArea(e.target.value)}
             required
